9fe320b3f94e191490c56fa35940ae00ddbf8018
saiakhil2103 <68911307+saiakhil2103@users.noreply.github.com>
2021-02-21 21:32:53 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -11,14 +11,20 @@ const addNewItem = () => {
 	display_list();
 }
 
-function display_list(){
+const display_list = () => {
 	const listReference = document.getElementById("to-do-list");
 	let display_text = "";
 
-	mylist.forEach((individualText) => {
-		let temp = `<li class="list-group-item d-flex justify-content-between align-items-center item">${individualText}</li>`
+	mylist.forEach((individualText, idx) => {
+		// let temp = `<li class="list-group-item d-flex justify-content-between align-items-center"><div>${individualText}<a href="#" onclick="deleteItem(${idx})" class="btn btn-outline-secondary"><i class="fa fa-minus-circle" aria-hidden="true"></i></a></div></li>`
+		// let temp = `<div class="input-group mb-3 task"><li class="list-group-item d-flex justify-content-between align-items-center form-control">${individualText}</li><button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fa fa-minus-circle" aria-hidden="true" onclick="deleteItem(${idx})"></i></button></li></div>`
+		let temp = `<div style="margin-top: 0px;" class="input-group mb-3 task"><li class="list-group-item d-flex justify-content-between align-items-center form-control">${individualText}</li><button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="fa fa-minus-circle" aria-hidden="true" onclick="deleteItem(${idx})"></i></button></li></div>`		
 		display_text += temp;
 	})
-
 	listReference.innerHTML = display_text;
+}
+
+const deleteItem = (idx) => {
+	mylist.splice(idx, 1);
+	display_list();
 }
